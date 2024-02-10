99db031bb510c2afcbc37ed21c1d67986035483a
Neeraj <neerajk7006@gmail.com>
2024-02-10 15:19:37 +0530

    React-Netflix-App

---
+++ netflixpage/src/Components/Cards.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Cards.css";
+
+const Card = (props) => {
+  return (
+    <>
+      <div className="cards">
+        <div className="card ">
+          <img src={props.imgscr} alt="myPic" className="card__img" />
+          <div className="card__info">
+            <span className="card__category"> {props.title}</span>
+            <h3 className="card__title">{props.sname}</h3>
+            <a href={props.links} target="_blank">
+              <button> Watch Now </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
---
+++ netflixpage/src/Sdata.js
@@ -0,0 +1,49 @@
+const Sdata = [
+  {
+    id: 1,
+    sname: "DARK",
+    imgscr: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 2,
+    sname: "extra curricular ",
+    imgscr:
+      "https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABccrTlUs3W2o8iCS_cjtkAZ6BX3a9av_xDti7KQmU5Iqe5StiDxIQfzWPfu82IMqGRsBXu1xwFOVk-qxoI0rpoH_GRhChjPgG5o7.jpg?r=8bf",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 3,
+    sname: "Stranger Things ",
+    imgscr:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyx10QeFLWSKh1VJVCtEvONA9W4XEYNtGtjzq5AejjNQ&s",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 4,
+    sname: "the Vampire Diaries ",
+    imgscr:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvm5UNobA7j2f4CsAX4Ndmwr11G1FMPwuGdJgNgHl7QQ&s",
+    title: "azon Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 5,
+    sname: "My First-2 Love",
+    imgscr:
+      "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/81026700?source=35",
+  },
+  {
+    id: 6,
+    sname: "Leo",
+    imgscr: "https://i.ytimg.com/vi/ShsFxGV_thk/maxresdefault.jpg",
+    title: "Netflix Original Series",
+    links: "https://www.netflix.com/in/title/81639323?preventIntent=true",
+  },
+];
+export default Sdata;
